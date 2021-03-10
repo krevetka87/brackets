bda72f5aa13f126395050716bc99b5f0aa76e44d
krevetka87 <bonanast4sia@yandex.ru>
2021-03-10 23:41:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pattern = bracketsConfig.map(el => el.join(''));
+  function cut(str) {
+    let newStr = str;
+    for (let elem of pattern) {
+      newStr = newStr.replace(elem, '')
+    }
+    if (str === newStr) {
+      return newStr;
+    }
+    else {
+      return cut(newStr);
+    }
+
+  }
+  return cut(str) === '';
+
 }
